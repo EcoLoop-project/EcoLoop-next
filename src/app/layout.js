7f0bca8b3ad41e615b6d89d5d969666d1238c8bc
farahk15 <faraharada15@gmail.com>
2026-05-09 10:15:03 +0300

    edite layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
     <html lang="ar" dir="rtl" suppressHydrationWarning>
       <body className="min-h-screen bg-slate-50 flex flex-col">
         <AppProvider>
-        
+         <Navbar />
           <main className="flex-grow w-full px-0 py-0">
             {children}
           </main>
